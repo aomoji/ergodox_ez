c3ec8e463e90bf6b4949e09865696dffbae809e4
aomoji <41809401+aomoji@users.noreply.github.com>
2020-05-24 18:34:46 +0900

    added a mod tap key.

--- keymap.c
+++ keymap.c
@@ -30,7 +30,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_NO,            KC_NO,        KC_NO,      KC_NO,       KC_NO,             KC_NO,        KC_NO,
     KC_LANG1,            KC_Y,         KC_U,       KC_I,        KC_O,              KC_P,         KC_BSPACE,
                       KC_H,         KC_J,       KC_K,        KC_L,              KC_SCOLON,    KC_ENTER,
-    KC_NO,            KC_N,         KC_M,       KC_COMMA,    KC_DOT,            KC_SLASH,     KC_QUOTE,
+    KC_NO,            KC_N,         KC_M,       KC_COMMA,    KC_DOT,            KC_SLASH,     SFT_T(KC_QUOTE),
                                     MO(1),      KC_RGUI,     RGUI(KC_SPACE),    RSFT(KC_TAB), KC_TAB,
     KC_NO,            KC_NO,
     KC_NO,
