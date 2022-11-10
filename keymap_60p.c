a9b6310eaf93f6af1683bd9d704767743553fc1e
aomoji <41809401+aomoji@users.noreply.github.com>
2022-11-10 17:58:43 +0900

    Update keymap_60p.c

--- keymap_60p.c
+++ keymap_60p.c
@@ -18,10 +18,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,           KC_Q,         KC_W,       KC_E,       KC_R,       KC_T,       KC_MINUS,
     KC_LCTRL,         KC_A,         KC_S,       KC_D,       KC_F,       KC_G,
     KC_LSHIFT,        KC_Z,         KC_X,       KC_C,       KC_V,       KC_B,       KC_LBRACKET,
-    LCTL(LGUI(KC_Q)), KC_NO,        KC_LALT,    KC_LGUI,    KC_LSHIFT,
+    KC_NO,            KC_NO,        KC_LALT,    KC_LGUI,    KC_LSHIFT,
     LGUI(LSFT(KC_4)),       LGUI(LSFT(LCTL(KC_4))), // スクリーンショット
     LGUI(LSFT(KC_5)), //スクリーンショット
-    LT(1, KC_SPACE),         MO(2),    KC_NO,
+    LT(1, KC_SPACE),         MO(2),    LCTL(LGUI(KC_Q)),
     // right
     KC_BSLASH,        KC_6,         KC_7,       KC_8,        KC_9,      KC_0,       KC_NO,
     KC_EQUAL,         KC_Y,         KC_U,       KC_I,        KC_O,      KC_P,       KC_BSPACE,
